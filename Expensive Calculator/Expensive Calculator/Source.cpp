d753852e066f3fd63e7ffe3262d3eba572aa0a3b
arichards558 <31348479+arichards558@users.noreply.github.com>
2018-03-29 13:33:22 -0500

    upload to main

--- Expensive Calculator/Expensive Calculator/Source.cpp
+++ Expensive Calculator/Expensive Calculator/Source.cpp
@@ -7,13 +7,44 @@
 
 int main()
 {
-	std::cout << "7 + 3 = " << 7.0 + 3.0 << std::endl;
-	std::cout << "7 - 3 = " << 7.0 - 3.0 << std::endl;
-	std::cout << "7 * 3 = " << 7.0 * 3.0 << std::endl;
-	std::cout << "7 / 3 = " << 7.0 / 3.0 << std::endl;
-	std::cout << "7 % 3 = " << 7 % 3 << std::endl;
-	std::cout << "7 + 3 * 5 = " << 7 + 3 * 5 << std::endl;
-	std::cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << std::endl;
+	int selection;
+	
+	double num1;
+	double num2;
+	double answer;
+
+	
+	std::cout << "Welcome to the super expensive calculator, Please enter the first number" << std::endl;
+	std::cin >> num1;
+	std::cout << "Enter your second number" << std::endl;
+	std::cin >> num2;
+	std::cout << "Please select an operation. 1 for addition, 2 for subtraction, 3 for division, 4 for multiplication, and 5 for modulus." << std::endl;
+	std::cin >> selection;
+	//Select which operation to use
+	if (selection == 1)
+	{
+		answer = num1 + num2;
+	}
+	if (selection == 2)
+	{
+		answer = num1 - num2;
+	}
+	if (selection == 3)
+	{
+		answer = num1 / num2;
+	}
+	if (selection == 4)
+	{
+		answer = num1 * num2;
+	}
+
+	if (selection >= 5)
+	{
+		std::cout << "This isnt an operation" << std::endl;
+	}
+	//Display answer
+	std::cout << "The answer is " << answer << std::endl;
+	
 	system("Pause");
 	return 0;
 }
